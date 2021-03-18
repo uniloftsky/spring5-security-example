f0760816a77175ccbd2ec5b1053d4ca1be911233
uniloftsky <uniloftsky@gmail.com>
2021-03-18 22:41:14 +0200

    project added

---
+++ src/main/java/com/uniloftsky/springframework/spring5securityexample/Spring5SecurityExampleApplication.java
@@ -0,0 +1,13 @@
+package com.uniloftsky.springframework.spring5securityexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5SecurityExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5SecurityExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniloftsky/springframework/spring5securityexample/Spring5SecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniloftsky.springframework.spring5securityexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5SecurityExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
